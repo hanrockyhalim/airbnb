2239930abcaa9a559565c8c862a233254f2eb2af
Hanrocky <hanrocky.halim@gmail.com>
2024-01-23 23:32:42 +0700

    Added styling to profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,13 +1,37 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
-import { useAuth } from "@clerk/clerk-expo";
+import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useAuth, useUser } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
 const Page = () => {
   const { signOut, isSignedIn } = useAuth();
+  const { user } = useUser();
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [email, setEmail] = useState(user?.emailAddresses[0].emailAddress);
+  const [edit, setEdit] = useState(false);
+
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+
+    setFirstName(user.firstName);
+    setLastName(user.lastName);
+    setEmail(user.emailAddresses[0].emailAddress);
+  }, [user]);
+
+  const onSaveUser = async () => {};
 
   return (
     <View>
+      <View style={styles.headerContainer}>
+        <Text>Profile</Text>
+
+        <Ionicons name="notifications-outline" size={26} />
+      </View>
       <Button title="log out" onPress={() => signOut()} />
       {!isSignedIn && (
         <Link href={"/(modals)/login"}>
@@ -17,5 +41,48 @@ const Page = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 24,
+  },
+  header: {
+    fontFamily: "mon-b",
+    fontSize: 24,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 24,
+    borderRadius: 16,
+    marginHorizontal: 24,
+    marginTop: 24,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    alignItems: "center",
+    gap: 14,
+    marginBottom: 24,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: Colors.grey,
+  },
+  editRow: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+});
 
 export default Page;
